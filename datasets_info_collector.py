0f9a0f1e0df72ecf9e184b49e40b4908cc79f9da
Lorrana Flores <83467821+lorranavf@users.noreply.github.com>
2024-01-17 11:44:59 -0300

    :ok_hand: revision and  :bug: fix

--- datasets_info_collector.py
+++ datasets_info_collector.py
@@ -1,7 +1,7 @@
 import os
 import time
 import requests
-import shutil
+import subprocess
 from tqdm import trange
 
 
@@ -15,58 +15,64 @@ class DatasetDownloader:
     Methods:
         check_internet(): Checks if there is an internet connection.
         download_datasets(): Downloads the datasets for the given accessions.
-        
-    """
-    def __init__(self, accessions):
-        self.accessions = accessions
 
-    def check_internet(self):
+    """
+
+    def __init__(self, accessions, outdir='in.datasets'):
+
+        if not isinstance(accessions, list):
+            raise ValueError("Accessions must be a list.")
+        
+        self.accessions = accessions
+        self.outdir = outdir
+
+    def is_internet(self):
         url = 'https://www.google.com'
-        timeout = 5
         try:
-            requests.get(url, timeout=timeout)
+            requests.get(url, timeout=5)
             return True
         except requests.exceptions.ConnectionError:
             return False
 
+    def check_internet(self):
+
+        while not self.is_internet():
+            time.sleep(5)
+
+    def is_accession_missing(self, acession):
+
+
+        return not os.path.exists(f'{self.outdir}/{acession}')
+
     def download_datasets(self):
-        
-        if os.path.exists('in.datasets'):
-            shutil.rmtree('in.datasets')
-        os.mkdir('in.datasets')
-        
-        error = []
-            
-            
-        for i in trange(len(self.accessions), ncols = 100):
-            internet = 'offline'
-            while internet == 'offline':
-                if not self.check_internet():
-                    internet = 'offline'
-                    time.sleep(10)
-                else:
-                    internet = 'online'
-                if internet == 'online':
-                    break
+                
+        print('Starting download.')
+
+        os.makedirs(self.outdir, exist_ok=True)
+
+        for i in trange(len(self.accessions), ncols=100):
+
+            self.check_internet()
+
+            while self.is_accession_missing(self.accessions[i]):
+
+                filezip = f'{self.outdir}/{self.accessions[i]}.zip'
+
+                cmd = [
+                    'datasets', 'download', 'genome', 'accession', self.accessions[i],
+                    '--include', 'gff3,rna,cds,protein,genome,seq-report',
+                    '--filename', filezip
+                ]
+
+                try:
+                    subprocess.run(cmd, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
                     
+                    if os.path.exists(filezip):
+                        subprocess.run(['unzip', '-qq', filezip, '-d', f'{self.outdir}/{self.accessions[i]}'], check=True)
+                        os.remove(filezip)
 
-            filezip = f'in.datasets/{self.accessions[i]}.zip'
+                except subprocess.CalledProcessError as e:
+                    print(f'Error downloading accession {self.accessions[i]}: {e}')
 
-            cmd = f'datasets download genome accession {self.accessions[i]} --include gff3,rna,cds,protein,genome,seq-report --filename {filezip} > /dev/null 2>&1'
-
-            os.system(cmd)
-
-            
-            if os.path.exists(filezip):
-                os.system(f'unzip -qq {filezip} -d in.datasets/{self.accessions[i]}')
-                os.remove(filezip)
-            else:
-                error.append(self.accessions[i])
-
-        if error:
-            print('The datasets for the following accessions were not downloaded:')
-            for accession in error:
-                print(accession)
-
-
-
+                if not self.is_accession_missing(self.accessions[i]):
+                    break
